2130d87b577dbd6c0b70ba79a61fd17222c2e37f
DrSpino <hicham_diouri@outlook.com>
2017-02-06 16:20:17 +0000

    Requete 3

--- TP1.sql
+++ TP1.sql
@@ -1,11 +1,18 @@
-SET ECHO ON
-/
-
-ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MM/YYYY'
-/
-
--- Req1
-SELECT  sigle, titre
-FROM    Inscription NATURAL JOIN Cours NATURAL JOIN Etudiant
-WHERE   nom = 'Tremblay' AND prenom = 'Jean'
-/
+ SET ECHO ON
+ /
+  
+ ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MM/YYYY'
+ /
+  
+ -- Req1
+ SELECT  sigle, titre
+ FROM    Inscription NATURAL JOIN Cours NATURAL JOIN Etudiant
+ WHERE   nom = 'Tremblay' AND prenom = 'Jean'
+ /
+ -- Req2
+ 
+ -- Req3
+ SELECT  sigle, noGroupe
+ FROM    Inscription NATURAL JOIN Etudiant NATURAL JOIN Cours
+ WHERE   nom = 'Tremblay' AND prenom = 'Lucie'AND codeSession = 32003
+ /
