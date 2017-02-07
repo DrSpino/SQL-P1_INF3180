578a0e4ae8f4c469fd06ea8251f1a1e197d6677f
Francois <franco.roussel@gmail.com>
2017-02-07 17:16:07 -0500

    Ajout d'une requête en SQL

--- TP1.sql
+++ TP1.sql
@@ -11,8 +11,20 @@
  /
  -- Req2
  
+ 
+ 
  -- Req3
  SELECT  sigle, noGroupe
  FROM    Inscription NATURAL JOIN Etudiant NATURAL JOIN Cours
  WHERE   nom = 'Tremblay' AND prenom = 'Lucie'AND codeSession = 32003
  /
+ 
+ --Req4
+ SELECT codePermanent, nom
+ FROM Etudiant NATURAL JOIN Inscription
+ WHERE sigle = 'INF3180' AND codeSession = 32003 AND dateAbandon IS NULL
+ /
+ 
+ --Req5
+
+ 
