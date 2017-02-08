45809ed9b06cc8b10bb1fab9798b0f0f1b61fb78
DrSpino <hicham_diouri@outlook.com>
2017-02-08 01:01:34 +0000

    Requete2 SQL

--- TP1.sql
+++ TP1.sql
@@ -1,30 +1,31 @@
  SET ECHO ON
  /
-  
+
  ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MM/YYYY'
  /
-  
+
  -- Req1
- SELECT  sigle, titre
- FROM    Inscription NATURAL JOIN Cours NATURAL JOIN Etudiant
- WHERE   nom = 'Tremblay' AND prenom = 'Jean'
+ SELECT sigle, titre
+ FROM   Inscription NATURAL JOIN Cours NATURAL JOIN Etudiant
+ WHERE  nom = 'Tremblay' AND prenom = 'Jean'
  /
+
  -- Req2
- 
- 
- 
- -- Req3
- SELECT  sigle, noGroupe
- FROM    Inscription NATURAL JOIN Etudiant NATURAL JOIN Cours
- WHERE   nom = 'Tremblay' AND prenom = 'Lucie'AND codeSession = 32003
+ SELECT codePermanent, prenom, nom
+ FROM   Inscription NATURAL JOIN Etudiant NATURAL JOIN SessionUQAM
+ WHERE  dateDebut <= '01/02/2004' AND dateFin >= '28/02/2004'
  /
- 
+
+ -- Req3
+ SELECT sigle, noGroupe
+ FROM   Inscription NATURAL JOIN Etudiant NATURAL JOIN Cours
+ WHERE  nom = 'Tremblay' AND prenom = 'Lucie'AND codeSession = 32003
+ /
+
  --Req4
  SELECT codePermanent, nom
- FROM Etudiant NATURAL JOIN Inscription
- WHERE sigle = 'INF3180' AND codeSession = 32003 AND dateAbandon IS NULL
+ FROM   Etudiant NATURAL JOIN Inscription
+ WHERE  sigle = 'INF3180' AND codeSession = 32003 AND dateAbandon IS NULL
  /
- 
- --Req5
 
- 
+ --Req5
